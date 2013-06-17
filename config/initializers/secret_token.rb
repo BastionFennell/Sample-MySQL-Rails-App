96aeffdcc15d28275a1d521c5e6d63e857d71445
Bastion <bastionfennell@utexas.edu>
2013-06-17 13:51:56 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeployTest::Application.config.secret_token = 'bfd14e26562de8697925b1ed155e1f063526bdb406908e607a64c04c7c79b4d03e26958c7f3050b5714738e351f04533cc74eff70e673ea380a5121fc802cfd4'
